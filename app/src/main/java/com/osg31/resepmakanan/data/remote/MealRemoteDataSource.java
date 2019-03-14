5c8e8a03b25ab59c9d87dadacce7dac0c2f35704
iqbalprabu <iqbalprabu14@gmail.com>
2019-03-14 23:21:43 +0700

    create base MVVM and Base Remote Data (#5)

---
+++ app/src/main/java/com/osg31/resepmakanan/data/remote/MealRemoteDataSource.java
@@ -0,0 +1,28 @@
+package com.osg31.resepmakanan.data.remote;
+
+import com.osg31.resepmakanan.data.MealDataSource;
+import com.osg31.resepmakanan.model.Meals;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MealRemoteDataSource implements MealDataSource {
+
+    private ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+    @Override
+    public void getAllMeals(String search, final GetMealCallback callback) {
+        Call<Meals> call = apiInterface.getAllMeals(search);
+        call.enqueue(new Callback<Meals>() {
+            @Override
+            public void onResponse(Call<Meals> call, Response<Meals> response) {
+                callback.onMealLoaded(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Meals> call, Throwable t) {
+                callback.onDataNotAvailable(t.toString());
+            }
+        });
+    }
+}
