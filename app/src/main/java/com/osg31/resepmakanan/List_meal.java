1c8298281d374e29cc72cda4c185897d1cda107f
Muhamad Fikri <46203393+Muhamadfikri20@users.noreply.github.com>
2019-03-15 21:40:35 +0700

    Origin/issue 4 (#7)
    * issue4
    
    * issue-4
    
    * tesissue-4
    
    * cobaissue-4

---
+++ app/src/main/java/com/osg31/resepmakanan/List_meal.java
@@ -0,0 +1,13 @@
+package com.osg31.resepmakanan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class List_meal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_meal);
+    }
+}
