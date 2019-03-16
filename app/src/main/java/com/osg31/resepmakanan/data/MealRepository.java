0af3700caa7b30ad94135897e632fbdc94d0bf90
Muhamad Fikri <46203393+Muhamadfikri20@users.noreply.github.com>
2019-03-16 16:51:35 +0700

    issue 9 (#15)

--- app/src/main/java/com/osg31/resepmakanan/data/MealRepository.java
+++ app/src/main/java/com/osg31/resepmakanan/data/MealRepository.java
@@ -1,5 +1,27 @@
 package com.osg31.resepmakanan.data;
 
-public class MealRepository {
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+import com.osg31.resepmakanan.data.remote.MealRemoteDataSource;
+import com.osg31.resepmakanan.model.Meals;
+public class MealRepository implements MealDataSource{
+    private MealRemoteDataSource mealRemoteDataSource;
+    public MealRepository(MealRemoteDataSource mealRemoteDataSource) {
+        this.mealRemoteDataSource = mealRemoteDataSource;
+    }
 
+    public void getAllMeals(final String search, final GetMealCallback callback) {
+        mealRemoteDataSource.getAllMeals(search,new GetMealCallback() {
+            @Override
+            public void onMealLoaded(Meals data) {
+                //insert  to Databa
+                callback.onMealLoaded(data);
+            }
+            @Override
+            public void onDataNotAvailable(String errorMessage) {
+                callback.onDataNotAvailable(errorMessage);
+            }
+        });
+    }
 }
