61ab0a369b108b5300c05918337bc9d380a97e16
iqbalprabu <iqbalprabu14@gmail.com>
2019-03-16 17:03:28 +0700

    create ListMealViewModel and Injection Class for create intance MealRepositiry

--- app/src/main/java/com/osg31/resepmakanan/Injection.java
+++ app/src/main/java/com/osg31/resepmakanan/Injection.java
@@ -1,8 +1,18 @@
 package com.osg31.resepmakanan;
 
+import android.content.Context;
+
+import com.osg31.resepmakanan.data.MealRepository;
+import com.osg31.resepmakanan.data.remote.MealRemoteDataSource;
+
 /**
  * Class for dependency injection
  *
  */
 public class Injection {
+
+    public static MealRepository provideMealRepository(Context context) {
+        return new MealRepository(new MealRemoteDataSource());
+    }
+
 }
