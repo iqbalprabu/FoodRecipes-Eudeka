c4100dfbe0b67588190c9a45eefe1e3aced26220
Alikhsan <alikhsan778@gmail.com>
2019-03-22 10:15:03 +0700

    Activity & layout list favorite recipe (#34)

---
+++ app/src/main/java/com/osg31/resepmakanan/view/ListMealFavActivity.java
@@ -0,0 +1,18 @@
+package com.osg31.resepmakanan.view;
+/*
+ * Create by Alikhsan on 3/21/2019.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.osg31.resepmakanan.R;
+
+public class ListMealFavActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_meal_fav);
+    }
+}
