32deecb50f8487a715f10dc7b9d70bb45b0777a0
Bambang Harianto Sianturi <bambangsianturi26@gmail.com>
2019-04-02 14:05:08 +0700

    Create Video Recipe 01/04/2019 (#40)

--- app/src/main/java/com/osg31/resepmakanan/view/MealDetailActivity.java
+++ app/src/main/java/com/osg31/resepmakanan/view/MealDetailActivity.java
@@ -1,5 +1,7 @@
 package com.osg31.resepmakanan.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
@@ -10,10 +12,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +26,7 @@ import com.osg31.resepmakanan.navigator.DeleteFavoriteNavigator;
 import com.osg31.resepmakanan.navigator.DetailMealNavigator;
 import com.osg31.resepmakanan.navigator.FindFavoriteNavigator;
 import com.osg31.resepmakanan.viewmodel.MealDetailViewModel;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,15 +34,26 @@ import butterknife.ButterKnife;
 public class MealDetailActivity extends AppCompatActivity implements DetailMealNavigator, AddFavoriteNavigator, DeleteFavoriteNavigator, FindFavoriteNavigator {
 
     public static final String DETAIL = "detail";
+//    private static final String YOUTUBE_THUMBNAIL = "https://img.youtube.com/vi/%s/mqdefault.jpg";
 
-    @BindView(R.id.tv_title_meal_detail) TextView tvTitleMeal;
-    @BindView(R.id.tv_category_meal_detail) TextView tvCategoryMeal;
-    @BindView(R.id.tv_description_meal_detail) TextView tvDescMeal;
-    @BindView(R.id.iv_poster_detail) ImageView ivPosterDetail;
-    @BindView(R.id.cl_detail_meal) CoordinatorLayout clDetailMeal;
-    @BindView(R.id.collapsing_detail) CollapsingToolbarLayout collapsingDetail;
-    @BindView(R.id.toolbar_detail) Toolbar toolbarDetail;
-    @BindView(R.id.fabFavorite) FloatingActionButton fabFavorite;
+    @BindView(R.id.tv_title_meal_detail)
+    TextView tvTitleMeal;
+    @BindView(R.id.tv_category_meal_detail)
+    TextView tvCategoryMeal;
+    @BindView(R.id.tv_description_meal_detail)
+    TextView tvDescMeal;
+    @BindView(R.id.iv_poster_detail)
+    ImageView ivPosterDetail;
+    @BindView(R.id.cl_detail_meal)
+    CoordinatorLayout clDetailMeal;
+    @BindView(R.id.collapsing_detail)
+    CollapsingToolbarLayout collapsingDetail;
+    @BindView(R.id.toolbar_detail)
+    Toolbar toolbarDetail;
+    @BindView(R.id.fabFavorite)
+    FloatingActionButton fabFavorite;
+    @BindView(R.id.recipe_video_thumbnail)
+    ImageView detailStepRecipe;
 
     private Menu collapsedMenu;
     private boolean appBarExpanded = true;
@@ -63,9 +75,9 @@ public class MealDetailActivity extends AppCompatActivity implements DetailMealN
         mealViewModel = new MealDetailViewModel(Injection.provideMealRepository(this),
                 Injection.provideFavoriteRepository(this));
 
+        mealViewModel.setFindFavoriteNavigator(this);
         mealViewModel.setAddFavoriteNavigator(this);
         mealViewModel.setDeleteFavoriteNavigator(this);
-        mealViewModel.setFindFavoriteNavigator(this);
         mealViewModel.setNavigator(this);
         mealViewModel.getDetailMeal(idMeal);
 
@@ -82,8 +94,8 @@ public class MealDetailActivity extends AppCompatActivity implements DetailMealN
         });
 
         fabFavorite.setOnClickListener(v -> {
-            Toast.makeText(this, "mealFavorite: " + isFavorite, Toast.LENGTH_SHORT).show();
-            if(isFavorite) mealViewModel.deleteFavorite(idMeal);
+//            Toast.makeText(this, "mealFavorite: " + isFavorite, Toast.LENGTH_SHORT).show();
+            if (isFavorite = false) mealViewModel.deleteFavorite(idMeal);
             else mealViewModel.addFavorite(mealDetail);
         });
     }
@@ -115,6 +127,17 @@ public class MealDetailActivity extends AppCompatActivity implements DetailMealN
         tvTitleMeal.setText(mealDetail.strMeal);
         tvCategoryMeal.setText(mealDetail.strCategory);
         tvDescMeal.setText(mealDetail.strInstructions);
+
+        Picasso.get()
+                .load(String.format(mealDetail.getStrMealThumb()))
+                .into(detailStepRecipe);
+
+        detailStepRecipe.setOnClickListener(v -> {
+            if (mealDetail != null && mealDetail.getStrYoutube() != null) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mealDetail.getStrYoutube()));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -122,13 +145,14 @@ public class MealDetailActivity extends AppCompatActivity implements DetailMealN
         finish();
         return super.onSupportNavigateUp();
     }
+
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         if (collapsedMenu != null
                 && (!appBarExpanded || collapsedMenu.size() != 1)) {
             //collapsed
 
-            if(isFavorite) {
+            if (isFavorite) {
                 collapsedMenu.add("Add")
                         .setIcon(R.drawable.ic_star_checked)
                         .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
@@ -138,7 +162,7 @@ public class MealDetailActivity extends AppCompatActivity implements DetailMealN
                         .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
             }
         } else {
-            if(isFavorite != null) {
+            if (isFavorite != null) {
                 if (isFavorite) {
                     fabFavorite
                             .setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_star_checked));
@@ -150,17 +174,18 @@ public class MealDetailActivity extends AppCompatActivity implements DetailMealN
         }
         return super.onPrepareOptionsMenu(collapsedMenu);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
-        collapsedMenu = menu;
+        this.collapsedMenu = menu;
         mealViewModel.checkIsMealFavorite(idMeal);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() != android.R.id.home) {
+        if (item.getItemId() != android.R.id.home) {
             if (isFavorite) mealViewModel.deleteFavorite(idMeal);
             else mealViewModel.addFavorite(mealDetail);
         } else finish();
